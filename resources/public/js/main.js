84983d10d85805535a6518c4a5ab1a6106265084
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-01-21 14:01:06 +0100

    Add some public files

---
+++ resources/public/js/main.js
@@ -0,0 +1,9 @@
+function writePort(val) {
+	$.get("/writePort", {value : val}, function(data) { $("#status").text(data); })
+		.fail(function() { $("#status").text("Failed to write port!"); });
+}
+
+function readPort() {
+	$.get("/readPort", {}, function(data) { $("#status").text(data); })
+		.fail(function() { $("#status").text("Failed to read port!"); });
+}
