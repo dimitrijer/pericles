55ba80fab1859a327e6f099025f924714e6c8360
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-01-21 14:32:16 +0100

    Read port on load

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,3 +1,5 @@
+$(function onReady() { readPort(); });
+
 function writePort(val) {
 	$.get("/writePort", {value : val}, function(data) { $("#status").text(data); })
 		.fail(function() { $("#status").text("Failed to write port!"); });
