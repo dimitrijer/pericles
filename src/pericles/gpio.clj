4975e74a3cf8c17d65ec717aac0a09331132b169
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-01-22 12:56:49 +0100

    Use swap! instead of reset! (not reading docs again -.-)

--- src/pericles/gpio.clj
+++ src/pericles/gpio.clj
@@ -9,25 +9,33 @@
   []
   (and (not (:debug? cfg)) (not (nil? @port))))
 
+(defn- open-port
+  [old-port new-port-num]
+  (let [open-new-port? (or
+                         (not old-port)
+                         (not= (:num old-port) new-port-num))]
+    (if (open-new-port?)
+      (do
+        (if old-port (gpio/close! (:port old-port)))
+        {:num new-port-num :port (gpio/open-port new-port-num :direction :out)})
+      old-port)))
+
 (defn init-port
   "Initializes GPIO port."
   []
   (if-not (:debug? cfg)
     (let [port-num (:gpio-port cfg)
-          new-port {:num port-num
-                    :port (gpio/open-port port-num :direction :out)}
-          old-port (reset! port new-port)]
-      (log/debugf "Opened GPIO port %d." port-num)
-      (if old-port (gpio/close! (:port old-port)))
-      new-port)))
+          new-port (swap! port open-port port-num)]
+      (log/debugf "Opened GPIO port %d." (:num new-port)))))
 
 (defn destroy-port
   "Closes GPIO port."
   []
-  (if-not (:debug? cfg)
-    (when-let [old-port (reset! port nil)]
-      (gpio/close! (:port old-port))
-      (log/debugf "Closed GPIO port %d." (:num old-port)))))
+  (when-not (:debug? cfg)
+    (swap! port (fn [old-port]
+                  (when old-port
+                    (gpio/close! (:port old-port))
+                    (log/debugf "Closed GPIO port %d." (:num old-port)))))))
 
 (defn read-port
   "Reads port value."
