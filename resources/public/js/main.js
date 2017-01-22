3d399e5d434ec9075cc29268b7b366fa3220b6cc
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-01-22 11:45:39 +0100

    Set status correctly in main.js

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,12 +1,12 @@
 $(function onReady() { readPort(); fetchTemperatureReadings(); });
 
 function writePort(val) {
-	$.get("/writePort", {value : val}, function(data) { $("#status").text(data); })
+	$.get("/writePort", {value : val}, function(data) { $("#status").text(data["status"]); })
 		.fail(function() { $("#status").text("Failed to write port!"); });
 }
 
 function readPort() {
-	$.get("/readPort", {}, function(data) { $("#status").text(data); })
+	$.get("/readPort", {}, function(data) { $("#status").text(data["status"]); })
 		.fail(function() { $("#status").text("Failed to read port!"); });
 }
 
