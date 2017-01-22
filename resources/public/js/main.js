eb848ce2f5e90a65e2e7a7342fc9147921b8d0e8
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-01-22 11:15:50 +0100

    Show temperature readings on the page

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,4 +1,4 @@
-$(function onReady() { readPort(); });
+$(function onReady() { readPort(); fetchTemperatureReadings(); });
 
 function writePort(val) {
 	$.get("/writePort", {value : val}, function(data) { $("#status").text(data); })
@@ -8,4 +8,16 @@ function writePort(val) {
 function readPort() {
 	$.get("/readPort", {}, function(data) { $("#status").text(data); })
 		.fail(function() { $("#status").text("Failed to read port!"); });
+}
+
+function fetchTemperatureReadings() {
+	$.get("/readTemp", {}, function(data) {
+		ul = $("#readings");
+		ul.html("");
+		ul.appendTo("body");
+		$(data).each(function (index, item) {
+			ul.append($(document.createElement("li"))
+				.text(item["name"] + ": " + item["celsius"] + "C"));
+		});
+	});
 }
